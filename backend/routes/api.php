7aabd3c798aa7a30ba1287eb283157dbb217e5aa
JulioCesarAnturiano <juliocesaranturianoeulate@gmail.com>
2026-01-27 17:09:46 -0400

    apis/sindicato/sindicatoRadioTaxi

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TrufiController;
 use App\Http\Controllers\TrufiRutaController;
+use App\Http\Controllers\SindicatoController;
+use App\Http\Controllers\SindicatoRadioTaxisController;
+
 Route::get('/test', function () {
     return response()->json([
         'success' => true,
@@ -17,9 +20,16 @@ Route::get('/test', function () {
 // ===========================
 // API PÚBLICA (FLUTTER) - SOLO LECTURA
 // ===========================
+//trufi normales
 Route::get('/trufis', [TrufiController::class, 'index']);
 Route::get('/trufis/{id}', [TrufiController::class, 'show']);
-
+//trufi rutas
 Route::get('/trufi-rutas', [TrufiRutaController::class, 'index']);
 Route::get('/trufi-rutas/{id}', [TrufiRutaController::class, 'show']);
 Route::get('/trufis/{idtrufi}/rutas', [TrufiRutaController::class, 'rutasPorTrufi']);
+// sindicato 
+Route::get('/sindicato', [SindicatoController::class, 'index']);
+Route::get('/sindicato/{id}', [SindicatoController::class, 'show']);
+//sindicato radio taxis
+Route::get('/sindicato', [SindicatoRadioTaxisController::class, 'index']);
+Route::get('/sindicato/{id}', [SindicatoRadioTaxisController::class, 'show']);
